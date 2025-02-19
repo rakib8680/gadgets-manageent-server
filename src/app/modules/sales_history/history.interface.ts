033859096023e1a078010639feedae1acd1afcfe
rakib8680 <rakib.cst.1st@gmail.com>
2025-02-19 18:39:09 +0600

    add Sales History interface

---
+++ src/app/modules/sales_history/history.interface.ts
@@ -0,0 +1,9 @@
+export type TSalesHistory = {
+  productId: number;
+  productName: string;
+  quantity: number;
+  buyerName: string;
+  buyerEmail: string;
+  totalAmount: number;
+  dateOfSale: string;
+};

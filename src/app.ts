e1fb759f5e7f4ef57fc97cd8091cabb600ffc7ba
rakib8680 <rakib.cst.1st@gmail.com>
2025-03-02 16:52:07 +0600

    Add api/route not found middleware

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import router from "./app/routes";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+import notFound from "./app/middlewares/notFound";
 const app: Application = express();
 
 // middlewares / parsers
@@ -16,8 +18,8 @@ app.get("/", (req: Request, res: Response) => {
 
 
 // error handlers
-// app.use(globalErrorHandler);
-// app.use(notFound);
+app.use(globalErrorHandler);
+app.use(notFound);
 
 
 

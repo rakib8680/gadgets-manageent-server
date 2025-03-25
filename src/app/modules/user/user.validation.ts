f88bc0c13b38aab4778132786009e4c9d28379a4
rakib8680 <rakib.cst.1st@gmail.com>
2025-03-25 17:39:42 +0600

    Refactor user validation schema

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -1,34 +1,36 @@
 import { z } from "zod";
 
-const createUserValidationSchema = z.object({
-  name: z
-    .string({
-      required_error: "Name is required",
-    })
-    .min(3, {
-      message: "Name must be atleast 3 characters",
-    })
-    .max(255, {
-      message: "Name must not exceed 255 characters",
-    }),
-  email: z
-    .string({
-      required_error: "Email is required",
-    })
-    .email({
-      message: "Invalid email format",
-    }),
-  password: z
-    .string({
-      required_error: "Password is required",
-    })
-    .min(4, {
-      message: "Password must be atleast 6 characters",
-    })
-    .max(20, {
-      message: "Password must not exceed 20 characters",
-    }),
-});
+const createUserValidationSchema = z
+  .object({
+    name: z
+      .string({
+        required_error: "Name is required",
+      })
+      .min(3, {
+        message: "Name must be atleast 3 characters",
+      })
+      .max(255, {
+        message: "Name must not exceed 255 characters",
+      }),
+    email: z
+      .string({
+        required_error: "Email is required",
+      })
+      .email({
+        message: "Invalid email format",
+      }),
+    password: z
+      .string({
+        required_error: "Password is required",
+      })
+      .min(4, {
+        message: "Password must be atleast 6 characters",
+      })
+      .max(20, {
+        message: "Password must not exceed 20 characters",
+      }),
+  })
+  .strict();
 
 export const UserValidations = {
   createUserValidationSchema,

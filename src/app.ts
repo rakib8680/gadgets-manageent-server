bf0b9795dda684d6777b7aef3926ac9c14b589a6
rakib8680 <rakib.cst.1st@gmail.com>
2025-08-14 19:10:37 +0600

    Enhance CORS configuration to allow specific methods and origins

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,12 @@ const app: Application = express();
 
 // middlewares / parsers
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "DELETE", "PATCH"],
+  })
+);
 
 // application route
 app.use("/api", router);
@@ -16,11 +21,8 @@ app.get("/", (req: Request, res: Response) => {
   res.send("hello world ");
 });
 
-
 // error handlers
 app.use(globalErrorHandler);
 app.use(notFound);
-
-
 
 export default app;

71c3385114edb60de4fdf85849352be4a5af29d7
rakib8680 <rakib.cst.1st@gmail.com>
2025-11-16 17:11:48 +0600

    Add user searchable fields constant to user constants file

--- src/app/modules/user/user.constant.ts
+++ src/app/modules/user/user.constant.ts
@@ -1,7 +1,7 @@
-import { TUserRole } from "./user.interface";
-
 export const USER_ROLE = {
   admin: "admin",
   seller: "seller",
   buyer: "buyer",
 } as const;
+
+export const userSearchableFields = ["name", "email"];
